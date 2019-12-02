275d82d8867ca2c124fbc78e3bfa080504591db3
Ryuujinx <admin@failverse.com>
2019-12-02 13:58:39 +0000

    Part1

--- day2/2-1.py
+++ day2/2-1.py
@@ -2,24 +2,24 @@
 
 def execute_opcode(intcode,pointer):
     if intcode[pointer] == 1:
-        intcode[pointer + 3 ] = int(intcode[pointer + 1]) + int(intcode[pointer + 2])
+        intcode[intcode[pointer + 3]] = int(intcode[intcode[pointer + 1]]) + int(intcode[intcode[pointer + 2]])
         status = True
     elif intcode[pointer] == 2:
-        intcode[pointer +3 ] = int(intcode[pointer + 1]) * int(intcode[pointer + 2])
+        intcode[intcode[pointer +3 ]] = int(intcode[intcode[pointer + 1]]) * int(intcode[intcode[pointer + 2]])
         status = True
     else:
         status = False
+    pointer = pointer + 4
     return intcode,pointer,status
 
 input = open("input", "r")
 intcode = input.read().split(",")
-incode = [ int(x) for x in intcode ]
+intcode = [ int(x) for x in intcode ]
 intcode[1] = 12
 intcode[2] = 2
 status = True
 pointer = 0
 while status:
     intcode,pointer,status = execute_opcode(intcode, pointer)
-    print(intcode)
 
 print(intcode[0])

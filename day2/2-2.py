b66e574a41a19ce3210fc626a0a28ec3c099b5c8
Ryuujinx <admin@failverse.com>
2019-12-02 14:47:10 +0000

    Day 2 finished

--- day2/2-2.py
+++ day2/2-2.py
@@ -6,14 +6,14 @@ def execute_opcode(intcode,pointer):
     if intcode[pointer] == 1:
         noun = int(intcode[intcode[pointer + 1]])
         verb = int(intcode[intcode[pointer + 2]])
+        value = noun + verb
         intcode[intcode[pointer + 3]] = noun + verb
-        check_pair(noun, verb, intcode[intcode[pointer + 3]])
         status = True
     elif intcode[pointer] == 2:
         noun = int(intcode[intcode[pointer + 1]])
         verb = int(intcode[intcode[pointer + 2]])
-        intcode[intcode[pointer + 3]] = noun * verb
-        check_pair(noun, verb, intcode[intcode[pointer + 3]])
+        value = noun * verb
+        intcode[intcode[pointer + 3]] = value
         status = True
     else:
         status = False
@@ -27,15 +27,27 @@ def check_pair(noun, verb, value):
 
 
 
-input = open("input", "r")
-intcode = input.read().split(",")
-intcode = [ int(x) for x in intcode ]
-intcode[1] = 12
-intcode[2] = 2
-status = True
-pointer = 0
-while status:
-    intcode,pointer,status = execute_opcode(intcode, pointer)
+noun = 0
+verb = 0
+last_verb = True
+
+while True:
+    input = open("input", "r")
+    intcode = input.read().split(",")
+    intcode = [ int(x) for x in intcode ]
+    intcode[1] = noun
+    intcode[2] = verb
+    status = True
+    pointer = 0
+    while status:
+        intcode,pointer,status = execute_opcode(intcode, pointer)
+    check_pair(noun, verb, intcode[0])
+    if noun == 99:
+        verb += 1
+        noun = 0
+    else:
+        noun += 1
+        
 
 
 print(intcode[0])

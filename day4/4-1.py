2b0368a3058682acfe561ef2a3e05bf22be2a5c9
Ryuujinx <admin@failverse.com>
2019-12-04 18:19:15 -0600

    Fixed thing. Still regex, can't stop me.

--- day4/4-1.py
+++ day4/4-1.py
@@ -9,7 +9,7 @@ paircount = 0
 for value in range(int(lowerlimit), int(upperlimit)):
     str_value = str(value)
     if re.search('(.)\\1', str_value):
-        if int(str(value)[0]) <= int(str_value[1]) and int(str_value[1]) <= int(str_value[2]) and int(str_value[2]) <= int(str_value[3]) and int(str_value[3]) <= int(str_value[4]) and int(str_value[4]) <= int(str_value[5]):
+        if int(str_value[0]) <= int(str_value[1]) and int(str_value[1]) <= int(str_value[2]) and int(str_value[2]) <= int(str_value[3]) and int(str_value[3]) <= int(str_value[4]) and int(str_value[4]) <= int(str_value[5]):
             paircount += 1
 
 print(paircount)
--- day4/4-2.py
+++ day4/4-2.py
@@ -9,7 +9,7 @@ paircount = 0
 for value in range(int(lowerlimit), int(upperlimit)):
     str_value = str(value)
     if re.search('(?<!1)11(?=[^1]|$)|(?<!2)22(?=[^2]|$)|(?<!3)33(?=[^3]|$)|(?<!4)44(?=[^4]|$)|(?<!5)55(?=[^5]|$)|(?<!6)66(?=[^6]|$)|(?<!7)77(?=[^7]|$)|(?<!8)88(?=[^8]|$)|(?<!9)99(?=[^9]|$)|(?<!0)00(?=[^0]|$)', str_value):
-        if int(str(value)[0]) <= int(str_value[1]) and int(str_value[1]) <= int(str_value[2]) and int(str_value[2]) <= int(str_value[3]) and int(str_value[3]) <= int(str_value[4]) and int(str_value[4]) <= int(str_value[5]):
+        if int(str_value[0]) <= int(str_value[1]) and int(str_value[1]) <= int(str_value[2]) and int(str_value[2]) <= int(str_value[3]) and int(str_value[3]) <= int(str_value[4]) and int(str_value[4]) <= int(str_value[5]):
             paircount += 1
 
 print(paircount)
